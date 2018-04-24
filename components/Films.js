c14356553d8f91288f1722bb6fcb160e4d81f49c
Sia <siakaramalegos@gmail.com>
2018-04-24 10:24:31 +0300

    Add films component

---
+++ components/Films.js
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import Container from './Container';
+
+const getIdFromUrl = (url) => {
+  const splitUrl = url.split('/')
+  return splitUrl[splitUrl.length - 2]
+}
+
+const Films = ({ films }) => {
+  const filmsList = films.map(film => {
+    const { title, opening_crawl, url } = film
+    const id = getIdFromUrl(url)
+
+    return (
+      <div className="film" key={id}>
+        <h2>{title}</h2>
+        <p>{opening_crawl}</p>
+        <p>
+          <Link href={`/films?id=${id}`}>
+            <a>Learn more...</a>
+          </Link>
+        </p>
+        <style jsx>{`
+          .film {
+            margin-top: 40px;
+          }
+        `}</style>
+      </div>
+    )
+  })
+
+  return (
+    <Container>
+      <h1>Films</h1>
+      {filmsList}
+    </Container>
+  )
+}
+
+export default Films
