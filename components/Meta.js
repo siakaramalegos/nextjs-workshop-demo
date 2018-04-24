6917f7cce6ea64c0943c2eedbf8082c886393468
Sia <siakaramalegos@gmail.com>
2018-04-24 12:19:03 +0300

    Adds loading indicator bar at top of page

--- components/Meta.js
+++ components/Meta.js
@@ -1,4 +1,14 @@
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
 
 export default () => (
   <div>
@@ -6,6 +16,7 @@ export default () => (
       <title>Star Wars is Cool"</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <link rel="stylesheet" type="text/css" href="../static/nprogress.css" />
     </Head>
     <style jsx global>{`
       body {
